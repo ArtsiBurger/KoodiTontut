f1fab09378c41053ae8fb8993b82ea6a32004447
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-11-17 14:22:24 +0200

    Kalenterin toiminnallissuutta

---
+++ scripts/functions.js
@@ -0,0 +1,31 @@
+function createCalendar() {
+    const calendar = document.querySelector(".calendar");
+
+    for (let i = 0; i < 24; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        const door = document.createElement("button");
+        door.classList.add("door");
+        door.textContent = (i+1);
+
+        const gift = document.createElement("div");
+        gift.classList.add("gift");
+
+        cell.appendChild(door);
+        cell.appendChild(gift);
+        calendar.appendChild(cell);
+        
+    }
+}
+
+
+function detectClick() {
+    const doors = document.querySelectorAll(".door");
+
+    doors.forEach(door => {
+        door.addEventListener("click", () => {
+            door.classList.add("open");
+        })
+    })
+}
---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+createCalendar();
+detectClick();
