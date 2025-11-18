137c31a5975f864c040ba3dd85820116349be5d5
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-11-18 10:10:43 +0200

    Numeroiden arpominen lisätty

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,5 +1,24 @@
-function createCalendar() {
+// Arpoo luukkujen numerot satunnaiseen järjestykseen
+function randomizeNumbers() {    
+    const numbers = [];
+
+    for (let i = 0; i < 24; i++) {
+        numbers.push(i+1);
+    }
+    // Fisher-Yates algoritmi
+    for (let i = numbers.length - 1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+    }
+    return numbers;
+}
+
+
+// Tekee kalenterin, luukut ja lahja
+function createCalendar() {   
     const calendar = document.querySelector(".calendar");
+
+    numbers = randomizeNumbers();
 
     for (let i = 0; i < 24; i++) {
         const cell = document.createElement("div");
@@ -7,21 +26,17 @@ function createCalendar() {
 
         const door = document.createElement("button");
         door.classList.add("door");
-        door.textContent = (i+1);
+        door.textContent = (numbers[i]);
         
-
         const gift = document.createElement("div");
         gift.classList.add("gift");
         
-
         cell.appendChild(door);
         cell.appendChild(gift);
-        calendar.appendChild(cell);
-        
+        calendar.appendChild(cell);   
     }
 }
-
-
+// Havaitsee käyttäjän syötteen
 function detectClick() {
     const doors = document.querySelectorAll(".door");
 
